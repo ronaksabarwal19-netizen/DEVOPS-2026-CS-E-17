58f33469057c9200cf6d839f21e703ade4963eac
Khush Sood <khushhhsood@gmail.com>
2026-08-24 00:58:04 +0530

    Add protected routes and dashboard layout

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { seedData } from './utils/localStorage'
+
+seedData();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
