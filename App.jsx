af89fddb09e99048669069504db8e97009455650
Ronak Sabarwal <ronaksabarwal19@gmail.com>
2026-08-25 23:13:19 +0530

    Add files via upload
    uploading files

---
+++ App.jsx
@@ -0,0 +1,39 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import ProtectedRoute from './components/layout/ProtectedRoute';
+import AppLayout from './components/layout/AppLayout';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Dashboard from './pages/Dashboard';
+import Accounts from './pages/Accounts';
+import Payments from './pages/Payments';
+import Cards from './pages/Cards';
+import Investments from './pages/Investments';
+import Settings from './pages/Settings';
+
+function Protected({ children }) {
+  return (
+    <ProtectedRoute>
+      <AppLayout>{children}</AppLayout>
+    </ProtectedRoute>
+  );
+}
+
+export default function App() {
+  return (
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/dashboard" element={<Protected><Dashboard /></Protected>} />
+          <Route path="/accounts" element={<Protected><Accounts /></Protected>} />
+          <Route path="/payments" element={<Protected><Payments /></Protected>} />
+          <Route path="/cards" element={<Protected><Cards /></Protected>} />
+          <Route path="/investments" element={<Protected><Investments /></Protected>} />
+          <Route path="/settings" element={<Protected><Settings /></Protected>} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+  );
+}
