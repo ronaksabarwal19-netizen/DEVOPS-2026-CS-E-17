b368315a34b3621f231c8fd0aa2525f429dc2594
Sarthak Navaria <navariyasarthak0910@gmail.com>
2026-08-31 02:15:42 +0530

    refactor: organize components

---
+++ front-end/src/components/dashboard/index.js
@@ -0,0 +1,5 @@
+﻿export { default as AccountCard } from './AccountCard';
+export { default as QuickActions } from './QuickActions';
+export { default as SpendingChart } from './SpendingChart';
+export { default as TransactionItem } from './TransactionItem';
+
